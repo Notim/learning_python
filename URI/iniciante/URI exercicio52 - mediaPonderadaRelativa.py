dbfc1fd2ab1f2d96305474bd993eac9e190de780
Notim <notim@noreply.com>
2018-08-29 00:01:14 -0300

    .

---
+++ URI/iniciante/URI exercicio52 - mediaPonderadaRelativa.py
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<iostream>
+#include <iomanip>
+def main():
+	entrada = int(input())
+	pond = []
+	pond = {2.0,3.0,5.0}
+	vector = [entrada],[4]
+
+	index = 0
+	for index in range(entrada):
+
+		position = 0
+		for position in range(3):
+			vector[index][position] = float(input())
+			vector[index][3] += (vector[index][position] * pond[position])
+
+		vector[index][3] = (vector[index][3] / (pond[0] + pond[1] + pond[2]))
+
+		print(vector[index][3])
+
+
+main()
+	
